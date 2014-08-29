503337fd6433cc5f76895afcd74062c629ef6195
Rafa Laguna <r@herding-cats.net>
2014-08-29 17:03:56 +0200

    Isolated $1 variable

--- capture_window.sh
+++ capture_window.sh
@@ -20,4 +20,4 @@ win_h=`echo $wininfo | grep "Height:" | cut -d : -f 2 | tr -d ' '`
 win_x=`echo $wininfo | grep "Absolute upper-left X:" | cut -d : -f 2 | tr -d ' '`
 win_y=`echo $wininfo | grep "Absolute upper-left Y:" | cut -d : -f 2 | tr -d ' '`
 
-avconv -f x11grab -r 30 -s "$win_w"x"$win_h" -i :0.0+"$win_x","$win_y+nomouse" -vcodec libx264 -pre lossless_ultrafast -crf 0 -threads 0 $1.mp4
+avconv -f x11grab -r 30 -s "$win_w"x"$win_h" -i :0.0+"$win_x","$win_y+nomouse" -vcodec libx264 -pre lossless_ultrafast -crf 0 -threads 0 "$1".mp4
