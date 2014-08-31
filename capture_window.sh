6d6bdd9f25d9e2a413bb8e55717bc3b93713b4b7
Rafa Laguna <r@herding-cats.net>
2014-08-31 17:39:51 +0200

    Compress after recording...

--- capture_window.sh
+++ capture_window.sh
@@ -20,4 +20,8 @@ win_h=`echo $wininfo | grep "Height:" | cut -d : -f 2 | tr -d ' '`
 win_x=`echo $wininfo | grep "Absolute upper-left X:" | cut -d : -f 2 | tr -d ' '`
 win_y=`echo $wininfo | grep "Absolute upper-left Y:" | cut -d : -f 2 | tr -d ' '`
 
+echo "Capturing..."
 avconv -f x11grab -r 30 -s "$win_w"x"$win_h" -i :0.0+"$win_x","$win_y+nomouse" -vcodec libx264 -pre lossless_ultrafast -crf 0 -threads 0 "$1".mp4
+
+echo "Compressing for editing..."
+avconv -i "$1".mp4 -strict experimental -f mp4 -vcodec libx264 -preset slow -crf 1 "$1"_compress.mp4
